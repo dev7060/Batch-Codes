05c275986a2301af542b09eb597c49987be551bf
BSi <46674147+mbhup@users.noreply.github.com>
2019-03-07 14:37:22 +0530

    13 complete, 14 pending

---
+++ 13/script.bat
@@ -0,0 +1,7 @@
+@echo off
+set variable=PRB
+echo This is a %variable%
+echo This is a %%variable%%
+cls
+echo ^<html^>
+echo ^^
---
+++ 14/script.bat
@@ -0,0 +1,7 @@
+@echo off
+set sum=1+1
+echo The sum is %sum%
+set /?
+cls
+set /a sum=1+1
+echo The sum is %sum%
